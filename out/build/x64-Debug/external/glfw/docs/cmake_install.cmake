b8ef14d08f4b9db99f122a7acf186614505d96b5
Cybercery <119060554+Cybercery@users.noreply.github.com>
2026-05-18 10:35:25 +0530

    fixed cmakelists error
    duplicate executable

--- out/build/x64-Debug/external/glfw/docs/cmake_install.cmake
+++ out/build/x64-Debug/external/glfw/docs/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/soham/Documents/graphics/openglstuff/external/glfw/docs
+# Install script for directory: C:/Users/soham/Projects/openglstuff/external/glfw/docs
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/soham/Documents/graphics/openglstuff/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/soham/Projects/openglstuff/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,6 +35,6 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/soham/Documents/graphics/openglstuff/out/build/x64-Debug/external/glfw/docs/install_local_manifest.txt"
+  file(WRITE "C:/Users/soham/Projects/openglstuff/out/build/x64-Debug/external/glfw/docs/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
