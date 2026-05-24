6f84987f2e0be6bc98681e57b0198dfaf7c99fa8
Cybercery <119060554+Cybercery@users.noreply.github.com>
2026-05-24 14:01:30 +0530

    Added texture wrapping
    added two textures
    mixed them, added color and wrapped with a rectanglle

--- balls.cpp
+++ balls.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 
 #include "shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 // Resize callback
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -44,20 +46,127 @@ int main()
         "../../../shaders/rainbow.frag"
     );
 
+    // colors
+    //float vertices[] = {
+    //    // positions        // colors
+    //     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+    //    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+    //     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    //};
+    // for textures
     float vertices[] = {
-        // positions        // colors
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+        // positions // colors // texture coords
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f // top left
+    };
+    
+    // texture
+
+    unsigned int texture1, texture2;
+
+    // texture1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned char* data =
+        stbi_load(
+            "../../../assets/brick.jpg",
+            &width,
+            &height,
+            &nrChannels,
+            0
+        );
+
+    if (data)
+    {
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RGB,
+            width,
+            height,
+            0,
+            GL_RGB,
+            GL_UNSIGNED_BYTE,
+            data
+        );
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << stbi_failure_reason() << std::endl;
+    }
+
+    stbi_image_free(data);
+
+
+    // texture2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    unsigned char* data2 =
+        stbi_load(
+            "../../../assets/awesomeface.png",
+            &width,
+            &height,
+            &nrChannels,
+            0
+        );
+
+    if (data2)
+    {
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RGBA,
+            width,
+            height,
+            0,
+            GL_RGBA,
+            GL_UNSIGNED_BYTE,
+            data2
+        );
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << stbi_failure_reason() << std::endl;
+    }
+
+    stbi_image_free(data2);
+
+    unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
     };
 
-    unsigned int VAO, VBO;
+    unsigned int VAO, VBO, EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
+    // Vertex buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glBufferData(
@@ -67,16 +176,25 @@ int main()
         GL_STATIC_DRAW
     );
 
+    // Element/index buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(indices),
+        indices,
+        GL_STATIC_DRAW
+    );
+
     // position attribute
     glVertexAttribPointer(
         0,
         3,
         GL_FLOAT,
         GL_FALSE,
-        6 * sizeof(float),
+        8 * sizeof(float),
         (void*)0
     );
-
     glEnableVertexAttribArray(0);
 
     // color attribute
@@ -85,12 +203,25 @@ int main()
         3,
         GL_FLOAT,
         GL_FALSE,
-        6 * sizeof(float),
+        8 * sizeof(float),
         (void*)(3 * sizeof(float))
     );
 
     glEnableVertexAttribArray(1);
+    // texture 
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(float),
+        (void*)(6 * sizeof(float))
+    );
+    glEnableVertexAttribArray(2);
 
+
+    shader.use();
+    shader.setInt("texture2", 1);
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -101,10 +232,24 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
+        
+        // makes it move left to right and right to left and left to right and right to left and left to right and right to left and left to right and right to left
+        //float timeValue = glfwGetTime();
 
+        //shader.setFloat(
+        //    "xOffset",
+        //    sin(timeValue) * 0.5f
+        //);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
         glBindVertexArray(VAO);
+        
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
--- include/shader.h
+++ include/shader.h
@@ -36,6 +36,7 @@ public:
         fragmentCode = fShaderStream.str();
 
         std::cout << vertexCode << std::endl;
+        std::cout << fragmentCode << std::endl;
 
         const char* vShaderCode = vertexCode.c_str();
         const char* fShaderCode = fragmentCode.c_str();
@@ -64,6 +65,19 @@ public:
     void use()
     {
         glUseProgram(ID);
+    }
+
+    void setBool(const std::string& name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+    void setInt(const std::string& name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
 };
 
